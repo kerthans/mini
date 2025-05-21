fa131851aa7fa4150bc94cda10ff362ff47e2766
Lumiat <grace20177102@icloud.com>
2025-05-21 10:23:02 +0800

    完成个人借物与团队借物页面

--- API/me.js
+++ API/me.js
@@ -1,6 +1,8 @@
 // utils/api.js (建议的独立接口文件路径)
 // const app = getApp()
-var config = wx.getStorageSync('config');
+const authToken = wx.getStorageSync('auth_token')
+
+
 /**
  * 通用用户数据获取方法
  * @param {Object} page 页面实例对象
@@ -9,7 +11,6 @@ var config = wx.getStorageSync('config');
  */
 function fetchUserData(page, successCallback, failCallback) {
   // 统一凭证校验
-  const authToken = wx.getStorageSync('auth_token')
   if (!authToken) {
     wx.showToast({ title: '请重新登录', icon: 'none' })
     wx.redirectTo({ url: '/pages/login/login' })
@@ -30,11 +31,11 @@ function fetchUserData(page, successCallback, failCallback) {
       
       if (res.statusCode === 200 && res.data.code === 200) {
         // 通用数据处理
-        const { username, phone_num, score, role } = res.data.data
+        const { real_name, phone_num, score, role } = res.data.data
         
         // 自动更新页面数据
         page.setData({
-          'userInfo.username': username,
+          'userInfo.username': real_name,
           'userInfo.phone_num': phone_num,
           'userInfo.score': score,
           'userInfo.role': role
